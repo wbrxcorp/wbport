f5b651de856f266b7e43778c1b3ac0140ee8db8f
Tomoatsu Shimada <shimada@walbrix.com>
2016-03-08 16:33:02 +0900

    a convinience DataSourceDefinition for MySQL added

--- src/main/scala/modules/database/DataSourceDefinition.scala
+++ src/main/scala/modules/database/DataSourceDefinition.scala
@@ -6,3 +6,9 @@ trait DataSourceDefinition {
   def password:String
   def connectionPoolSettings:scalikejdbc.ConnectionPoolSettings = scalikejdbc.ConnectionPoolSettings()
 }
+
+trait MySQLDataSourceDefinition extends DataSourceDefinition {
+  def url:String = "jdbc:mysql://%s/%s?useUnicode=true&characterEncoding=UTF8&zeroDateTimeBehavior=convertToNull&useCompression=true&autoReconnect=true&socketTimeout=10000".format(hostname, dbname)
+  def hostname:String = "localhost"
+  def dbname:String
+}
