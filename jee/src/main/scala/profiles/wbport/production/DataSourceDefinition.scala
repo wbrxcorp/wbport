1a65884566c1a5bddbe5e3e1b2927a92c6972a3e
Tomoatsu Shimada <shimada@walbrix.com>
2016-03-15 19:00:36 +0900

    stop using mysql compat mode

--- jee/src/main/scala/profiles/wbport/production/DataSourceDefinition.scala
+++ jee/src/main/scala/profiles/wbport/production/DataSourceDefinition.scala
@@ -3,7 +3,7 @@ package profiles.wbport.production
 object DataSourceDefinition extends modules.database.DataSourceDefinition {
   private val projectName = buildinfo.BuildInfo.name
 
-  def url:String = "jdbc:h2:/var/lib/tomcat7/db/wbport;MVCC=true;;AUTO_SERVER=true;DB_CLOSE_DELAY=-1;MODE=MySQL;INIT=CREATE SCHEMA IF NOT EXISTS \"public\""
+  def url:String = "jdbc:h2:/var/lib/tomcat7/db/wbport;MVCC=true;;AUTO_SERVER=true;DB_CLOSE_DELAY=-1"
   def user:String = "sa"
   def password:String = ""
 }
