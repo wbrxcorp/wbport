187b29e0482e8777966f1c005971400a0c81bc59
Tomoatsu Shimada <shimada@walbrix.com>
2016-06-21 10:10:39 +0900

    MySQLDataSourceDefinition to support specifying timezone

--- jee/src/main/scala/modules/database/DataSourceDefinition.scala
+++ jee/src/main/scala/modules/database/DataSourceDefinition.scala
@@ -8,7 +8,13 @@ trait DataSourceDefinition {
 }
 
 trait MySQLDataSourceDefinition extends DataSourceDefinition {
-  def url:String = "jdbc:mysql://%s/%s?useUnicode=true&characterEncoding=UTF8&zeroDateTimeBehavior=convertToNull&useCompression=true&autoReconnect=true&socketTimeout=10000".format(hostname, dbname)
+  def url:String = {
+    "jdbc:mysql://%s/%s?useUnicode=true&characterEncoding=UTF8&zeroDateTimeBehavior=convertToNull&useCompression=true&autoReconnect=true&socketTimeout=10000".format(hostname, dbname) + (timezone match {
+      case Some(timezone) => "&useLegacyDatetimeCode=false&serverTimezone=%s".format(timezone)
+      case None => ""
+    })
+  }
   def hostname:String = "localhost"
   def dbname:String
+  def timezone:Option[String] = None
 }
